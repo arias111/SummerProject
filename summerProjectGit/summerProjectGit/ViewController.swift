2fb67808c21240217bc1a955535e02c370901618
arias111 <46973859+arias111@users.noreply.github.com>
2020-07-10 00:57:26 -0700

    Add files via upload

---
+++ summerProjectGit/summerProjectGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  summerProjectGit
+//
+//  Created by Nail on 10.07.2020.
+//  Copyright © 2020 Nail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//1
+
+}
+
